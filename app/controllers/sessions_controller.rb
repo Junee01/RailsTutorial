c15159db12ae861f2c3d1a76e1d88893c434fb7a
Junee01 <potter777777@naver.com>
2016-08-16 19:00:28 +0900

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		log_in user
+  		redirect_to user	#redirect_to user_url(user)
+  	else
+  		flash.now[:danger] = '일치하는 회원정보가 없습니다. 다시 확인 부탁드립니다.'
+  		render 'new'
+  	end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,17 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user #회원가입 후 바로 로그인
   		flash[:success] = "성공적으로 회원가입이 되었습니다."
   		redirect_to @user #redirect_to user_url(@user)와 같은 의미이다.
   	else
   		render 'new'
   	end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 
   private
