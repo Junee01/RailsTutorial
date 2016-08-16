351b8d8d5f871115384011507d69a39cd8af77c3
Junee01 <potter777777@naver.com>
2016-08-16 18:03:35 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+  def show
+  	@user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "성공적으로 회원가입이 되었습니다."
+  		redirect_to @user #redirect_to user_url(@user)와 같은 의미이다.
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end
 end
