8a149792151928206f0f469e5b5f476e9d731613
Junee01 <potter777777@naver.com>
2016-08-17 17:48:39 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		redirect_to user	#redirect_to user_url(user)
+  		redirect_back_or user
   	else
   		flash.now[:danger] = '일치하는 회원정보가 없습니다. 다시 확인 부탁드립니다.'
   		render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -18,9 +27,24 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "개인 정보가 수정되었습니다."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
-    log_out
-    redirect_to root_url
+    User.find(params[:id]).destroy
+    flash[:success] = "계정이 성공적으로 삭제되었습니다."
+    redirect_to users_url
   end
 
   private
@@ -28,4 +52,24 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "로그인 후 사용가능합니다."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
