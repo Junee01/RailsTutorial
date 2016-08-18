992e5158de82d1513247780eefcd432d96bc07fb
Junee01 <potter777777@naver.com>
2016-08-18 19:12:42 +0900

    removed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,33 @@
-User.create!(name:  "Junee01",
-             email: "sangjunpark0203@gmail.com",
-             password:              "sangjun101",
-             password_confirmation: "sangjun101",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
+# User.create!(name:  "Example01",
+#              email: "example@gmail.com",
+#              password:              "12345678",
+#              password_confirmation: "12345678",
+#              admin: true,
+#              activated: true,
+#              activated_at: Time.zone.now)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password,
+#                activated: true,
+#                activated_at: Time.zone.now)
+# end
 
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   content = Faker::Lorem.sentence(5)
+#   users.each { |user| user.microposts.create!(content: content) }
+# end
 
 # Following relationships
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+# users = User.all
+# user  = users.first
+# following = users[2..50]
+# followers = users[3..40]
+# following.each { |followed| user.follow(followed) }
+# followers.each { |follower| follower.follow(user) }
